9044ece26dd798d66d30c77623097fba8eca1270
Nathan Arnold <natearn@natearn.info>
2017-12-10 15:17:42 +0100

    Add pure, unstyled Cart component
    I'm just shoving it into the default App to see that it renders
    properly.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Cart from './Cart.js'
+
+const dummyData = {
+    title: "A Title",
+    products: [1,2,3,4].map(id => ({
+        id: id,
+        name: "Product " + id,
+        comment: "This is a product comment that is long enough to exceed the size of its container.",
+        price: 12.50,
+        tax: 0.11,
+    })),
+    add: () => console.log("ADD"),
+    remove: ({name}) => console.log("REMOVE " + name),
+    clear: () => console.log("CLEAR"),
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +29,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Cart {...dummyData} />
       </div>
     );
   }
---
+++ src/Cart.js
@@ -0,0 +1,62 @@
+import React from "react"
+
+const Cart = ({
+	title,
+	products,
+	add,
+	remove,
+	clear,
+}) => (
+	<section>
+		<header>
+			<h1>{title}</h1>
+			<button onClick={add}>Add</button>
+			<button onClick={clear}>Clear</button>
+		</header>
+		<table>
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Name</th>
+					<th>Comments</th>
+					<th>Price</th>
+					<th>Tax</th>
+					<th>Total</th>
+				</tr>
+			</thead>
+			<tbody>
+				{products.map((item,idx) => (
+					<CartRow
+						key={item.id}
+						index={idx}
+						remove={() => remove(item)}
+						{...item}
+					/>
+				))}
+			</tbody>
+		</table>
+	</section>
+)
+
+const calcTotal = (price,tax) => price * (1 + tax)
+
+const CartRow = ({
+	index,
+	remove,
+	name,
+	comment,
+	price,
+	tax,
+}) => (
+	<tr>
+		<td>{index}</td>
+		<td>{name}</td>
+		<td>{comment}</td>
+		<td>{price}</td>
+		<td>{tax}</td>
+		<td>{calcTotal(price,tax)}</td>
+		<td><button onClick={remove}>×</button></td>
+	</tr>
+)
+
+export default Cart
