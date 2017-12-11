06f11ef3ba67e8ce20c9f5a20a2520c0e09e6c99
Nathan Arnold <natearn@natearn.info>
2017-12-11 14:38:42 +0100

    Update smoke test for Cart
    Some definitions changed. I've also formatted the file consistently

--- src/Cart.test.js
+++ src/Cart.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Cart from './Cart';
+import React from 'react'
+import { render } from 'react-dom'
+import CartApp,{ Cart } from './Cart'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(Cart(), div);
-});
+test('the pure cart renders without crashing', () => {
+	const div = document.createElement('div')
+	render(<Cart />, div);
+})
